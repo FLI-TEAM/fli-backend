5a75424ece6e47158c3f05a51495ebc4e47fc721
sunghyeonjo <sunghyeon_jo@naver.com>
2022-10-22 16:11:56 +0900

    Feat : 스프링부트 프로젝트 생성

---
+++ src/main/java/team/fli/FliApplication.java
@@ -0,0 +1,13 @@
+package team.fli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/fli/FliApplicationTests.java
@@ -0,0 +1,13 @@
+package team.fli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
